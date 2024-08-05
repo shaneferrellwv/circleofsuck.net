4d614d15efae62fa1e92acefdd56796d0b4d4e6f
shaneferrellwv <shaneferrell@ufl.edu>
2024-08-05 15:47:14 -0400

    fix import typo

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import logo from '../images/logo.png';
-import './Header.css';
+import './header.css';
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
