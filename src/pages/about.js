6057d0bc6de976ca1dccba9f412ea9b512957e0b
shaneferrellwv <shaneferrell@ufl.edu>
2024-08-07 17:59:26 -0400

    update about info (add instagram and fix typo)

--- src/pages/about.js
+++ src/pages/about.js
@@ -23,9 +23,10 @@ const AboutPage = () => {
         The Traveling Salesman Problem states: Given a list of cities and the distances between each pair of cities,
         what is the shortest possible route (otherwise known as a <em>Hamiltonian cycle</em>) that visits each city exactly once and returns to the origin city?
         </p>
-        <p>The website is driven by a bot that periodically pulls sport leagues & scores data using a public sports API to find any <em>Hamiltonian cycles</em> within leagues, conferences, and divisions. You can find the bot on Twitter at <a className='link' href="http://x.com/circleofsuck">@circleofsuck</a>.</p>
+        <p>The website is driven by a bot that periodically pulls sport leagues & scores data using a public sports API to find any <em>Hamiltonian cycles</em> within leagues, conferences, and divisions.
+          You can find the bot on <a className='link' href="http://x.com/circleofsuck">Twitter</a> and <a className='link' href="http://instagram.com/circleofsuck">Instagram</a> at @circleofsuck.</p>
 
-        <p>Becuase finding a Circle of Suck is a form of the Traveling Salesman Problem, we can apply any algorithm used to solve the Traveling Salesman Problem to find a circle of suck.</p>
+        <p>Because finding a Circle of Suck is a form of the Traveling Salesman Problem, we can apply any algorithm used to solve the Traveling Salesman Problem to find a circle of suck.</p>
         <p>Unfortunately, the Traveling Salesman Problem falls under the class of NP-hard computational problems.
           This means that mathematicians and scientists haven't been able to find an algorithm that is accurate 100% of the time to solve these problems.</p>
         <p>Instead, we are stuck using algorithms that provide the optimal solution most of the time. For a trivial problem like finding a Circle of Suck, this is perfectly fine.
